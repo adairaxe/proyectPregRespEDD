c2c54880251eb4a85a8472c95a4950a602c53763
adairaxe <aabrigo@espol.edu.ec>
2022-08-22 01:22:11 -0500

    Hay problemas para diseñar la interfaz

---
+++ proyectPreguntasEDD/src/main/resources/ec/edu/espol/proyectpreguntasedd/PrimaryController.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.proyectpreguntasedd;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+/**
+ * FXML Controller class
+ *
+ * @author USER
+ */
+public class PrimaryController implements Initializable {
+
+
+    @FXML
+    private ImageView menos;
+    @FXML
+    private ImageView mas;
+    @FXML
+    private Label num_preguntas;
+    @FXML
+    private Button cargarButton;
+    @FXML
+    private Button playButton;
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
--- proyectPreguntasEDD/src/main/resources/ec/edu/espol/proyectpreguntasedd/SecondaryController.java
+++ proyectPreguntasEDD/src/main/resources/ec/edu/espol/proyectpreguntasedd/SecondaryController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model;
+package ec.edu.espol.proyectpreguntasedd;
 
 import java.io.File;
 import java.net.URL;
