c6673a9048d96c432f52cd4979db4712ddac31b1
adairaxe <aabrigo@espol.edu.ec>
2022-08-22 20:52:41 -0500

    Interfaz de preguntas casi completa

--- proyectPreguntasEDD/src/main/java/ec/edu/espol/proyectpreguntasedd/InicioController.java
+++ proyectPreguntasEDD/src/main/java/ec/edu/espol/proyectpreguntasedd/InicioController.java
@@ -7,7 +7,11 @@ package ec.edu.espol.proyectpreguntasedd;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 
 /**
  * FXML Controller class
@@ -15,6 +19,15 @@ import javafx.fxml.Initializable;
  * @author USER
  */
 public class InicioController implements Initializable {
+
+    @FXML
+    private Label lb_pregunta;
+    @FXML
+    private RadioButton rbt_no;
+    @FXML
+    private RadioButton rbt_si;
+    @FXML
+    private Button bt_siguiente;
 
     /**
      * Initializes the controller class.
